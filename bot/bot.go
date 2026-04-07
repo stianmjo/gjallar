06a85dc11d8ba90b75696e631a991369ba728db6
Stian Johnsen <stianmjo@stud.ntnu.no>
2026-04-07 19:11:18 +0200

    fixed

--- bot/bot.go
+++ bot/bot.go
@@ -10,9 +10,8 @@ import (
 	"gjallar/commands"
 )
 
-// RUn initializes and starts the Discord bot called from main.go
+// Run initializes and starts the Discord bot called from main.go
 func Run(token string) {
-
 
 	// Create a new Discord session with the bot token
 	dg, err := discordgo.New("Bot " + token)
@@ -36,20 +35,23 @@ func Run(token string) {
 	// Close the connection when the function returns
 	defer dg.Close()
 
-	// Create a channel to recieve OS signals
+	log.Println("Gjallar is running. Press Ctrl+C to exit.")
+
+	// Create a channel to receive OS signals
 	sc := make(chan os.Signal, 1)
 	// Listen for interrupt (Ctrl+C)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
 	<-sc
+}
 
-	func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+// messageHandler is called every time a message is created
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-		// Ignore messages from the bot itself to prevent loops
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		// Pass the message to our command handler
-		commands.Handle(s, m)
+	// Ignore messages from the bot itself to prevent loops
+	if m.Author.ID == s.State.User.ID {
+		return
 	}
+
+	// Pass the message to our command handler
+	commands.Handle(s, m)
 }
